ab747674d0698aec86cd6a3c86bd680b6edaae96
EleRova <elenarovacchi99@gmail.com>
2023-12-22 11:30:35 +0100

    Driver con login funzionante

--- PROGETTO_DRIVER/settings.py
+++ PROGETTO_DRIVER/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "DRIVER.apps.DriverConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,8 +55,7 @@ ROOT_URLCONF = 'PROGETTO_DRIVER.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'DRIVER/templates']
-        ,
+        'DIRS': [BASE_DIR / 'DRIVER/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
--- PROGETTO_DRIVER/urls.py
+++ PROGETTO_DRIVER/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for PROGETTO_DRIVER project.
+URL configuration for PCCapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic.base import TemplateView  # new
 
 urlpatterns = [
+    path('DRIVER/', include("DRIVER.urls")),
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
