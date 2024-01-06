31649b631b53eae3c80830e6eac49f6c03cbebea
EleRova <elenarovacchi99@gmail.com>
2024-01-06 12:33:14 +0100

    Temperature

--- DELIVER/admin.py
+++ DELIVER/admin.py
@@ -1,8 +1,9 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Driver, Market, Trip
+from .models import Driver, Market, Trip, Temperature
 
 admin.site.register(Driver)
 admin.site.register(Market)
 admin.site.register(Trip)
+admin.site.register(Temperature)
