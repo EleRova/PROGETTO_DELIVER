4d646c4e638bb3f76c3fcdde2688f15dc0a05506
EleRova <elenarovacchi99@gmail.com>
2024-01-10 10:34:55 +0100

    Tentativo

--- PROGETTO_DELIVER/settings.py
+++ PROGETTO_DELIVER/settings.py
@@ -111,8 +111,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    "static"]
 
-STATIC_URL = 'DELIVER/static/'
+STATIC_URL = 'static/'
+
+#STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
